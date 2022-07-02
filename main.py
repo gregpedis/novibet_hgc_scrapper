85e8c5c58c6772d4d0df5e1a63b5780b0b6d6e50
gregpedis <trafalgar1618@hotmail.com>
2022-07-02 22:21:50 +0300

    Fixed some shit

--- main.py
+++ main.py
@@ -4,12 +4,18 @@ from hgc.filesystem import helper as fs
 
 
 def main():
+    print("cleaning the directory...")
     fs.clean_fs()
+    print("fetching the main page...")
     web_scr.get_main_page()
+    print("scrapping the xsd_urls...")
     web_scr.get_xsd_urls()
+    print("fetching the xsds...")
     web_scr.get_xsds()
+    print("extracting assertions from xsds...")
     ass_scr.extract_assertions()
 
 
 if __name__ == "__main__":
     main()
+
